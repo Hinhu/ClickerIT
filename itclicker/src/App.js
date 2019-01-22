760cd8fe4890237691b244be74d3fa80310ce26e
Maciek <maciekz1997@gmail.com>
2019-01-22 22:36:24 +0100

    Added Material UI package and created app header

--- itclicker/src/App.js
+++ itclicker/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import IDEToolBar from './IDEToolBar.js';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <button>CLICK</button>
+      <div >
+        <header>
+          <IDEToolBar />
         </header>
+        <Grid container spacing={0} justify="center" alignItems="stretch" style={{paddingTop:'62px'}}>
+          <Grid item md={3}>
+          </Grid>
+          <Grid item md={7}>
+          </Grid>
+          <Grid item md={2}>
+          </Grid>
+        </Grid>
       </div>
     );
   }
---
+++ itclicker/src/IDEToolBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+class IDEToolBar extends Component {
+    render() {
+        return (
+            <div >
+                <AppBar style={{ background: '#2b2727' }}>
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit" >
+                            IT Clicker
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+export default IDEToolBar;
